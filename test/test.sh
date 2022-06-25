9e3b136ee8087c2b19688bc78906b01794de63f1
Alex Hall <alex.mojaki@gmail.com>
2022-06-26 00:35:25 +0200

    Give servers time to start?

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,7 @@ webpack --mode production
 python server.py &
 TEST_SERVER_PORT=8001 TEST_SERVER_HTTPS=1 python server.py &
 
+sleep 3
 curl -k http://localhost:8000
 curl -k https://localhost:8001
 
