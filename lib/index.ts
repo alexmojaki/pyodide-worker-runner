b3ac50e970674f0fc0b9da86a9938431803a76dc
Alex Hall <alex.mojaki@gmail.com>
2022-09-25 17:55:07 +0200

    0.21.3

--- lib/index.ts
+++ lib/index.ts
@@ -13,7 +13,7 @@ export interface PackageOptions {
   extractDir?: string;
 }
 
-export async function defaultPyodideLoader(version: string = "0.21.2") {
+export async function defaultPyodideLoader(version: string = "0.21.3") {
   const indexURL = `https://cdn.jsdelivr.net/pyodide/v${version}/full/`;
   const result = await loadPyodide({indexURL});
   if (result.version !== version) {
