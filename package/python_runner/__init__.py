088691e019340b9f8fac81c21eb8ce5b751b17fb
clems <clemens.s.bachmann@gmail.com>
2024-08-15 10:23:36 +0200

    try with pyodide 23.4

--- package/python_runner/__init__.py
+++
@@ -1,7 +0,0 @@
-from .runner import Runner, PatchedStdinRunner, PatchedSleepRunner, PyodideRunner
-
-try:
-    from .version import __version__
-except ImportError:  # pragma: no cover
-    # version.py is auto-generated with the git tag when building
-    __version__ = "???"
